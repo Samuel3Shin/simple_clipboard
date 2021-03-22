a299f2a557c9991f107af3dcfe7c266e9a22c538
Samuel3Shin <samuel3.shin@gmail.com>
2021-03-22 22:49:45 +0900

    put admob info in ad_manager.dart

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -1,45 +1,12 @@
 import 'dart:io';
 import 'dart:io' show Platform;
-import 'package:firebase_admob/firebase_admob.dart';
 
 class AdManager {
-  BannerAd _bannerAd;
-
-  String appID = Platform.isIOS
-      ? 'ca-app-pub-2551410065860924~3350581677' // iOS Test App ID
-      : 'ca-app-pub-2551410065860924~7132439238'; // Android Test App ID
-  String bannerID = BannerAd.testAdUnitId;
-  String interstitialID = InterstitialAd.testAdUnitId;
-
-  static MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
-    keywords: <String>['flutter', 'firebase', 'admob'],
-    testDevices: <String>[],
-  );
-
-  init() async {
-    FirebaseAdMob.instance.initialize(appId: appID);
-    _bannerAd = createBannerAd();
-    _bannerAd
-      ..load()
-      ..show();
-  }
-
-  BannerAd createBannerAd() {
-    return BannerAd(
-      adUnitId: bannerID,
-      size: AdSize.banner,
-      targetingInfo: targetingInfo,
-      listener: (MobileAdEvent event) {
-        print("BannerAd event is $event");
-      },
-    );
-  }
-
   static String get appId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544~4354546703";
+      return "ca-app-pub-2551410065860924~7132439238";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544~2594085930";
+      return "ca-app-pub-2551410065860924~3350581677";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
@@ -47,31 +14,31 @@ class AdManager {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/8865242552";
+      return "ca-app-pub-2551410065860924/7044809324";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4339318960";
+      return "ca-app-pub-2551410065860924/4123132510";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
   }
 
-  static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/7049598008";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/3964253750";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
+  // static String get interstitialAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "ca-app-pub-3940256099942544/7049598008";
+  //   } else if (Platform.isIOS) {
+  //     return "ca-app-pub-3940256099942544/3964253750";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
 
-  static String get rewardedAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/8673189370";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/7552160883";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
+  // static String get rewardedAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "ca-app-pub-3940256099942544/8673189370";
+  //   } else if (Platform.isIOS) {
+  //     return "ca-app-pub-3940256099942544/7552160883";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
 }
