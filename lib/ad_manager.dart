29b8dcd1f31d949a286cf9e5308868453da8fa3f
Samuel3Shin <samuel3.shin@gmail.com>
2021-03-29 21:07:02 +0900

    release 1.0.1 optimized loading listView and added firebase admob initialization

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -4,11 +4,11 @@ import 'dart:io' show Platform;
 class AdManager {
   static String get appId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544~4354546703";
-      // return "ca-app-pub-2551410065860924~7132439238";
+      // return "ca-app-pub-3940256099942544~4354546703";
+      return "ca-app-pub-2551410065860924~7132439238";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544~2594085930";
-      // return "ca-app-pub-2551410065860924~3350581677";
+      // return "ca-app-pub-3940256099942544~2594085930";
+      return "ca-app-pub-2551410065860924~3350581677";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
@@ -16,11 +16,11 @@ class AdManager {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/8865242552";
-      // return "ca-app-pub-2551410065860924/7044809324";
+      // return "ca-app-pub-3940256099942544/8865242552";
+      return "ca-app-pub-2551410065860924/7044809324";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4339318960";
-      // return "ca-app-pub-2551410065860924/4123132510";
+      // return "ca-app-pub-3940256099942544/4339318960";
+      return "ca-app-pub-2551410065860924/4123132510";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
